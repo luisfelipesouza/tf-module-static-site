a6d0c45639207a4168f9ea682356bb521c69c0cf
Ribeiro, Luis Felipe <luisfelipe.ribeiro@eventim.com.br>
2022-05-01 22:56:37 -0300

    added bucket_name output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output endpoint {
   value       = aws_s3_bucket_website_configuration.website.website_endpoint
 }
+output bucket_name {
+  value       = aws_s3_bucket.website.ic
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,3 @@
-variable bucket_name {
-  type      = string
-}
 variable index_document {
   type      = string
   default   = "index.html"

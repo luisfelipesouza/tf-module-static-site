c9eb730bedde70b5ed35e19b5cc0d7efd439d121
Ribeiro, Luis Felipe <luisfelipe.ribeiro@eventim.com.br>
2022-05-01 22:57:48 -0300

    fixed bucket_name output

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output endpoint {
   value       = aws_s3_bucket_website_configuration.website.website_endpoint
 }
 output bucket_name {
-  value       = aws_s3_bucket.website.ic
+  value       = aws_s3_bucket.website.id
 }
